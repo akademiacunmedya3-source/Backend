951a26cf0365f42a9f79a29cd9f213c518e569d7
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-01-28 22:52:22 +0300

    feat(persistence): add GameResult entity mapping
    * Implemented mapping for GameResult entity in Entity Framework.
    * Defined indexes and relationships for improved data integrity.

---
+++ Harfistan/Infrastructure/Harfistan.Persistence/EntityMappings/GameResultMapping.cs
@@ -0,0 +1,31 @@
+using Harfistan.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Harfistan.Persistence.EntityMappings;
+
+public class GameResultMapping : IEntityTypeConfiguration<GameResult>
+{
+    public void Configure(EntityTypeBuilder<GameResult> builder)
+    {
+        builder.ToTable("GameResults");
+        builder.HasKey(gr => gr.Id);
+
+        builder.Property(gr => gr.GuessesJson).IsRequired().HasDefaultValue("[]");
+        builder.Property(gr => gr.DeviceType).HasMaxLength(20);
+
+        builder.HasIndex(gr => new { gr.UserId, gr.DailyWord }).IsUnique();
+        builder.HasIndex(gr => gr.PlayedAt);
+        builder.HasIndex(gr => gr.IsWin);
+        
+        builder.HasOne(gr => gr.User)
+            .WithMany(u => u.GameResults)
+            .HasForeignKey(gr => gr.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        builder.HasOne(gr => gr.DailyWord)
+            .WithMany(dw => dw.GameResults)
+            .HasForeignKey(gr => gr.DailyWordId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
