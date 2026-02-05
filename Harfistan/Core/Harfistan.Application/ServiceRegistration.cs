9bfa516e79ced1366a7e73139d7aaad7b0e1ca03
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-02-05 21:54:09 +0300

    feat(dependencies): add FluentValidation and Mediator packages
    * Added FluentValidation and its extensions for validation support
    * Integrated Mediator for better command and query handling

--- Harfistan/Core/Harfistan.Application/ServiceRegistration.cs
+++ Harfistan/Core/Harfistan.Application/ServiceRegistration.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Harfistan.Application;
@@ -6,7 +8,8 @@ public static class ServiceRegistration
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        
+        services.AddMediator(options => options.ServiceLifetime = ServiceLifetime.Scoped);
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         return services;
     }
-}
+}  
