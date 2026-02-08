1cb659b46c92c23f69f10cc07b0816da0aa384da
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-02-08 19:01:23 +0300

    refactor(UserSessionRepository): improve method structure and add ordering
    * Refactored methods for better readability and consistency
    * Added ordering to GetActiveSessionsAsync for improved session retrieval

--- Harfistan/Infrastructure/Harfistan.Persistence/Repositories/UserSessionRepository.cs
+++ Harfistan/Infrastructure/Harfistan.Persistence/Repositories/UserSessionRepository.cs
@@ -5,12 +5,23 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Harfistan.Persistence.Repositories;
 
-public class UserSessionRepository(HarfistanDbContext _context)  : IUserSessionRepository
+public class UserSessionRepository(HarfistanDbContext _context) : IUserSessionRepository
 {
     public DbSet<UserSession> Table => _context.Set<UserSession>();
-    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) => await _context.SaveChangesAsync(cancellationToken); 
-    public async Task<UserSession> AddAsync(UserSession session, CancellationToken cancellationToken = default) => Table.AddAsync(session, cancellationToken).Result.Entity;
-    public async Task<List<UserSession>> GetActiveSessionsAsync(Guid userId, CancellationToken cancellationToken = default) => await Table.Where(x => x.UserId == userId && x.IsActive).ToListAsync(cancellationToken);
-    public async Task<UserSession?> GetByIdAsync(int id, CancellationToken cancellationToken = default) => await Table.FindAsync(id, cancellationToken);
-    public async Task UpdateAsync(UserSession session, CancellationToken cancellationToken = default) => Table.Update(session);
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) =>
+        await _context.SaveChangesAsync(cancellationToken);
+
+    public async Task<UserSession> AddAsync(UserSession session, CancellationToken cancellationToken = default) =>
+        Table.AddAsync(session, cancellationToken).Result.Entity;
+
+    public async Task<List<UserSession>> GetActiveSessionsAsync(Guid userId,
+        CancellationToken cancellationToken = default) =>
+        await Table.Where(x => x.UserId == userId && x.IsActive).OrderByDescending(s => s.LoginAt).ToListAsync(cancellationToken);
+
+    public async Task<UserSession?> GetByIdAsync(int id, CancellationToken cancellationToken = default) =>
+        await Table.FirstOrDefaultAsync(s => s.Id == id, cancellationToken);
+
+    public async Task UpdateAsync(UserSession session, CancellationToken cancellationToken = default) =>
+        Table.Update(session);
 }
